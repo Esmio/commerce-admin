57c60ee18b0e98abde9b4458e0b7dcde05ddd076
sailbirds <sailbirds@luminesence.jp>
2023-07-18 01:02:32 +0900

    product

---
+++ components/navbar.tsx
@@ -0,0 +1,31 @@
+import { UserButton, auth } from '@clerk/nextjs';
+import React from 'react';
+import { redirect } from 'next/navigation';
+
+import { MainNav } from '@/components/main-nav';
+import StoreSwitcher from '@/components/store-switcher';
+import prismadb from '@/lib/prismadb';
+
+export default async function navbar() {
+  const { userId } = auth();
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+  return (
+    <div className="border-b">
+      <div className="flex items-center h-16 px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="flex items-center ml-auto space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { ImagePlus, Trash } from 'lucide-react';
+import Image from 'next/image';
+import { CldUploadWidget } from 'next-cloudinary';
+
+interface ImageUpaloadProps {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+const ImageUpdload: React.FC<ImageUpaloadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value,
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div className="flex items-center gap-4 mb-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
+          >
+            <div className="absolute z-10 top-2 right-2">
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                variant="destructive"
+                size="icon"
+              >
+                <Trash className="w-4 h-4" />
+              </Button>
+            </div>
+            <Image fill className="object-cover" alt="Image" src={url} />
+          </div>
+        ))}
+      </div>
+      <CldUploadWidget onUpload={onUpload} uploadPreset="p3ajsfs1">
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
+          return (
+            <Button
+              onClick={onClick}
+              type="button"
+              disabled={disabled}
+              variant="secondary"
+            >
+              <ImagePlus className="w-4 h-4 mr-2" />
+              Upload an Image
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
+};
+
+export default ImageUpdload;
